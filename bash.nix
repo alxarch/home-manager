fe6be0689d888dc3d42be1cfe54195c382058610
Alexandros Sigalas <alexandros.sigalas@gmail.com>
2024-02-04 11:37:49 +0200

    Force interactive behavior on delete/overwrite

--- bash.nix
+++ bash.nix
@@ -8,20 +8,40 @@
     enableCompletion = true;
     enableVteIntegration = true;
     historyControl = [ "ignorespace" "ignoredups" ];
-    historyIgnore = [ "ls" "cd" "exit"];
-    shellAliases = {
-      ".." = "cd ..";
-      "g" = "git";
-      "rm" = "echo 'rm is disabled. Use trash of rmi'";
-      "rmi" = "${pkgs.coreutils}/bin/rm -i";
-    };
-    initExtra = ''
-          # enable vi editing
-          set -o vi
+    historyIgnore = [ "ls" "cd" "exit" ];
+    shellAliases =
+      # Wrap destructive file operations to force --interactive
+      let wrapInteractive = { name, ... }:
+        let
+          app = pkgs.writeShellApplication {
+            inherit name;
+            runtimeInputs = with pkgs; [ coreutils appendToVar ];
+            text = ''
+              args="''$@"
+              appendToVar args --interactive
+              ${name} ''$args
+            '';
+          }; in
+        "${app}/bin/${name}";
+      in
 
-          # Create a directory and cd into it. 
-          # Makes a termporary directory if no name is provided
-          function mkcd { if [ -z "$1" ]; then cd "$(mktemp -d)"; else mkdir -p "$1" && cd "$1"; fi }
+      {
+        ".." = "cd ..";
+        "g" = "git";
+        "rm" = wrapInteractive "rm";
+        "mv" = wrapInteractive "mv";
+      };
+    initExtra = ''
+      # enable vi editing
+      set -o vi
+      # This will raise an error if you try to overwrite an existing file by output redirection.
+      # You can force the redirection to work with special syntax: 
+      #   $ echo "Foo" >| existing_file
+      set -o noclobber
+
+      # Create a directory and cd into it. 
+      # Makes a termporary directory if no name is provided
+      function mkcd { if [ -z "$1" ]; then cd "$(mktemp -d)"; else mkdir -p "$1" && cd "$1"; fi }
     '';
   };
 }
