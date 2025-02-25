bd51ac15ac0f4104677352cf33144af4c46997b2
Alexandros Sigalas <alexandros.sigalas@gmail.com>
2025-02-25 12:01:52 +0200

    Add better git defaults

--- git.nix
+++ git.nix
@@ -8,9 +8,28 @@
     enable = true;
     package = pkgs.gitAndTools.gitFull;
     extraConfig = {
-      pull.rebase = true;
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
       rerere.enabled = true;
+      rerere.autoupdate = true;
       init.defaultBranch = "main";
+      push.default = "simple";
+      push.autoSetupRemote = true;
+      push.followTags = true;
+      pull.rebase = true;
+      pull.ff = "only";
+      fetch.prune = true;
+      fetch.pruneTags = true;
+      fetch.all = true;
+
+      rebase.autoSquash = true;
+      rebase.autoStash = true;
+      # Move refs of stacked branches above when rebasing 
+      rebase.updateRefs = true;
+
+      help.autocorrect = "prompt";
+      log.date = "iso";
     };
     userEmail = "alexandros.sigalas@gmail.com";
     userName = "Alexandros Sigalas";
